9cf632fdb3c59c31e32f24beae92180899d5df0c
KJ <kjloving1400@gmail.com>
2023-01-31 15:18:08 -0800

    finished swagger

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
-    has_many :facts
+    has_many :facts, dependent: :destroy
   end
